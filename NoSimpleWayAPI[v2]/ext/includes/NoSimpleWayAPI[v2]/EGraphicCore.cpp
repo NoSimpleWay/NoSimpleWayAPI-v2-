cbaaa4b69952d49cc416f2c58ba709fdcd484259
User <User@USER-PC>
2023-03-15 08:48:08 +0300

    improve basalt style

--- NoSimpleWayAPI[v2]/ext/includes/NoSimpleWayAPI[v2]/EGraphicCore.cpp
+++ NoSimpleWayAPI[v2]/ext/includes/NoSimpleWayAPI[v2]/EGraphicCore.cpp
@@ -1761,7 +1761,7 @@ void NS_EGraphicCore::create_styles()
 		NS_EGraphicCore::load_style_texture(just_created_style, jc_brick);
 
 		EBrickStyle::set_border_size(jc_brick, 3.0f, 3.0f, 3.0f, 3.0f);
-		EBrickStyle::set_offset_size(jc_brick, 3.0f, 3.0f, 3.0f, 3.0f);
+		EBrickStyle::set_offset_size(jc_brick, 6.0f, 6.0f, 6.0f, 6.0f);
 		EBrickStyle::set_subdivisions(jc_brick, 2, 2);
 
 		just_created_style->brick_style[BrickStyleID::GROUP_MAIN] = *jc_brick;
