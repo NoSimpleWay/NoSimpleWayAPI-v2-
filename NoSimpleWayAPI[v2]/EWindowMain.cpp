cbf9a1353c2f5c26a1262552c996993a9f37b664
NoSimpleWay <12486248624p>
2025-09-02 11:55:18 +0300

    Change texture of "Abyssal signet" Change version

--- NoSimpleWayAPI[v2]/EWindowMain.cpp
+++ NoSimpleWayAPI[v2]/EWindowMain.cpp
@@ -6159,7 +6159,7 @@ void EWindowMain::read_user_loot_patterns()
 }
 
 
-const std::string				EWindowMain::this_version = "1.7.1";
+const std::string				EWindowMain::this_version = "1.7.2";
 std::vector<ELocalisationText>	EWindowMain::registered_transfigured_gems;
 
 
